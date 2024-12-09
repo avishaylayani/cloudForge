6b42c6e4e4e2d0aa531d759594ee046d18490727
avishaylayani <166747220+avishaylayani@users.noreply.github.com>
2024-12-09 19:39:50 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,17 +17,17 @@ private_key_url="https://tinyurl.com/ypm9nkwc"
 cleaning_secret_key="gpg --batch --yes --delete-secret-key $secret_key_id 2> /dev/null" # saved commadn that deletes the private key from registry - used multiple time in the script
 
 # For BASH script, import the private key to be able to decrypt the values file & Deletes sensitive data from local (whether imported succeded or faild)
-( curl -s -L $private_key_url -o private.key  && gpg --import private.key 2> /dev/null && rm -rf private.key && echo "[+] importing key successfully") || \
+( curl -s -L $private_key_url -o /home/ubuntu/application_files/private.key  && gpg --import private.key 2> /dev/null && rm -rf /home/ubuntu/application_files/private.key && echo "[+] importing key successfully") || \
 ( echo "[-] Something went wrong with importing private key, exiting" && rm -rf private.key && eval "$cleaning_secret_key" &&  exit 1 )
 
 # Decrypting the values file, to a one with decrypted values (Removing installed private key whether decryption succeded or fails )
-( sops -d values_encrypted_dev.yaml > details_app_dev/values.yaml && sops -d values_encrypted_prod.yaml > details_app_prod/values.yaml && eval "$cleaning_secret_key" && echo "[+] values file decrypted successfully") || \
+( sops -d /home/ubuntu/application_files/values_encrypted_dev.yaml > /home/ubuntu/application_files/details_app_dev/values.yaml && sops -d /home/ubuntu/application_files/values_encrypted_prod.yaml > /home/ubuntu/application_files/details_app_prod/values.yaml && eval "$cleaning_secret_key" && echo "[+] values file decrypted successfully") || \
 ( echo "[-] Something went wrong with decryption process, exiting" && eval "$cleaning_secret_key" && exit 1 )   
 
 # Deploy details_app using Helm - deletes values file 
-( helm install details-app-prod details_app_prod && rm -rf details_app_prod/values.yaml && echo "[+] Production Deployment succeded" && \
- helm install details-app-dev details_app_dev && rm -rf details_app_dev/values.yaml && echo "[+] Dev Deployment succeded" ) || \
-( echo "[-] Something went wrong with installing helm chart, existing" && rm -rf details_app/values.yaml && exit 1 )
+( helm install details-app-prod /home/ubuntu/application_files/details_app_prod && rm -rf /home/ubuntu/application_files/details_app_prod/values.yaml && echo "[+] Production Deployment succeded" && \
+ helm install details-app-dev /home/ubuntu/application_files/details_app_dev && rm -rf /home/ubuntu/application_files/details_app_dev/values.yaml && echo "[+] Dev Deployment succeded" ) || \
+( echo "[-] Something went wrong with installing helm chart, existing" && rm -rf /home/ubuntu/application_files/details_app/values.yaml && exit 1 )
 
 
 
